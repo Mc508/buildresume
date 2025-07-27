34dcd566b62de27f740e912e7041adc90bdfbd90
Mc508 <mihirc508@gmail.com>
2025-07-27 16:24:19 +0530

    frontend setup done

---
+++ frontend/src/App.tsx
@@ -0,0 +1,8 @@
+const App = () => {
+  return (
+    <div className="my-auto min-h-screen">
+      <h1 className="text-4xl text-amber-200 text-center items-center">App</h1>
+    </div>
+  );
+};
+export default App;
